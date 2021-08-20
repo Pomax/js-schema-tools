c678d05e8abb04bc2154493dc6ecc4dcac5aebcb
Pomax <pomax@nihongoresources.com>
2021-08-20 10:58:49 -0700

    .

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ import * as equals from "./lib/equals/equals.js";
 import * as schema from "./lib/schema/basic-js-schema.js";
 import * as store from "./lib/store/json-data-store.js";
 
-export default { diff, equals, schema, store };
+import * as html from "./lib/schema/forms/create-html.js";
+import * as tree from "./lib/schema/forms/create-tree.js";
+
+const TOOLS = { diff, equals, schema, store, forms: { html, tree } };
+
+export default TOOLS;
