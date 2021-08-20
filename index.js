b2997c31e2b075f1147ff61fb7c62ecdc8fb3d4d
Pomax <pomax@nihongoresources.com>
2021-08-20 11:16:22 -0700

    .

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import * as diff from "./lib/diff/diff.js";
 import * as equals from "./lib/equals/equals.js";
 import * as schema from "./lib/schema/basic-js-schema.js";
-import * as store from "./lib/store/json-data-store.js";
+import * as datastore from "./lib/store/json-data-store.js";
 
 import * as html from "./lib/schema/forms/create-html.js";
 import * as tree from "./lib/schema/forms/create-tree.js";
 
 const forms = { html, tree };
 
-export { diff, equals, schema, store, forms };
+export { diff, equals, schema, datastore, forms };
