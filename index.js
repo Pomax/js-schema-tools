d223e654f44b3e332770e46868b3ce79f0401715
Pomax <pomax@nihongoresources.com>
2021-08-20 11:24:03 -0700

    labelfunction

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import * as datastore from "./lib/store/json-data-store.js";
 
 import * as html from "./lib/schema/forms/create-html.js";
 import * as tree from "./lib/schema/forms/create-tree.js";
+import labelFunction from "./lib/schema/forms/label-function.js";
 
-const forms = { html, tree };
+const forms = { html, tree, labelFunction };
 
 export { diff, equals, schema, datastore, forms };
