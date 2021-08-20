c96e996295453c4bf674d274844b8f357afad015
Pomax <pomax@nihongoresources.com>
2021-08-20 10:55:00 -0700

    lib entry

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as diff from "./lib/diff/diff.js";
+import * as equals from "./lib/equals/equals.js";
+import * as schema from "./lib/schema/basic-js-schema.js";
+import * as store from "./lib/store/json-data-store.js";
+
+export default { diff, equals, schema, store };
