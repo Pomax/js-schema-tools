79fe3a6e4ac975fa75054e529679b327fcdb0f29
Pomax <pomax@nihongoresources.com>
2021-10-02 10:31:38 -0700

    reset with payload

--- test/models/model.stored.test.js
+++ test/models/model.stored.test.js
@@ -200,6 +200,25 @@ describe(`Testing User model with store backing`, () => {
     expect(() => user.save()).not.toThrow();
   });
 
+  test(`Model resetting with a post-reset payload works as expected`, () => {
+    const { name, password } = user.profile;
+    user.profile.preferences.config.end_of_hand_timeout = 0;
+    user.profile.preferences.avatar = `empty.png`;
+    user.profile.reset({
+      preferences: {
+        avatar: "New Avatar.png",
+        config: {
+          end_of_hand_timeout: 0,
+        },
+      },
+    });
+    expect(user.profile.preferences.config.end_of_hand_timeout).toBe(0);
+    expect(user.profile.preferences.avatar).toBe(`New Avatar.png`);
+    expect(user.profile.name).toBe(name);
+    expect(user.profile.password).toBe(password);
+    expect(() => user.save()).not.toThrow();
+  });
+
   test(`Setting "config.player_count" to false is a validation error (direct)`, () => {
     expect(() => {
       user.profile.preferences.config.player_count = false;
--- test/models/model.unstored.test.js
+++ test/models/model.unstored.test.js
@@ -18,15 +18,48 @@ describe(`Testing User model without store backing`, () => {
     Models.setStore(undefined);
   });
 
+  let user;
+
+  beforeEach(() => {
+    user = User.create(testData);
+  });
+
   test(`Can create User model without a store backing`, () => {
-    const user = User.create(testData);
     expect(user.profile.name).toBe(testData.profile.name);
     expect(user.profile.password).toBe(testData.profile.password);
   });
 
   test(`Submodels work as standalone models`, () => {
-    const user = User.create(testData);
     expect(user.toHTMLTable().slice(0, 6)).toBe(`<table`);
     expect(user.profile.toHTMLTable().slice(0, 6)).toBe(`<table`);
+  });
+
+  test(`Model resetting works as expected`, () => {
+    const { name, password } = user.profile;
+    user.profile.preferences.config.end_of_hand_timeout = 0;
+    user.profile.preferences.avatar = `empty.png`;
+    user.profile.reset();
+    expect(user.profile.preferences.config.end_of_hand_timeout).toBe(10000);
+    expect(user.profile.preferences.avatar).toBe(undefined);
+    expect(user.profile.name).toBe(name);
+    expect(user.profile.password).toBe(password);
+  });
+
+  test(`Model resetting with a post-reset payload works as expected`, () => {
+    const { name, password } = user.profile;
+    user.profile.preferences.config.end_of_hand_timeout = 0;
+    user.profile.preferences.avatar = `empty.png`;
+    user.profile.reset({
+      preferences: {
+        avatar: "New Avatar.png",
+        config: {
+          end_of_hand_timeout: 0,
+        },
+      },
+    });
+    expect(user.profile.preferences.config.end_of_hand_timeout).toBe(0);
+    expect(user.profile.preferences.avatar).toBe(`New Avatar.png`);
+    expect(user.profile.name).toBe(name);
+    expect(user.profile.password).toBe(password);
   });
 });
